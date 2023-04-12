d9ce75bd1d1b866ee004d273fd3ed16180d30f4b
omaryaya <omar.yaya.work@gmail.com>
2023-04-12 05:47:56 +0200

    CORS config

--- practiceapp/src/main/java/com/omaryaya/practiceapp/exception/GlobalExceptionHandler.java
+++ practiceapp/src/main/java/com/omaryaya/practiceapp/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.NoSuchElementException;
 
 
 @RestControllerAdvice
@@ -17,15 +18,9 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(NotFound.class)
+    @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<String> handleNotFound(RuntimeException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
-    }
-
-    class NotFound extends RuntimeException {
-        public NotFound(String message) {
-            super(message);
-        }
     }
 }
